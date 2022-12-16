031ac0b0caef4c24b5ea83b63587e7dc2065ed2d
younhwan97 <younhwan0903@gmail.com>
2022-12-17 03:28:35 +0900

    [Apply] .gitignore

--- app/src/main/java/kr/co/younhwan/simpleloadingdialog/SimpleLoading.kt
+++ app/src/main/java/kr/co/younhwan/simpleloadingdialog/SimpleLoading.kt
@@ -1,25 +1,16 @@
 package kr.co.younhwan.simpleloadingdialog
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import com.airbnb.lottie.compose.*
 
